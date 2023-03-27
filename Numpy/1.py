9378a05644c20be918d28f6e22394e64b4bd06f2
Priyanshu <hellohyy838@gmail.com>
2023-03-27 13:30:56 +0530

    VS Python

---
+++ Numpy/1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+print(pd.__version__)
+# In terminal --- pip3 show pandas
+
+import numpy as np
+print(np.__version__)
+print(np.__file__)
+# In terminal --- pip3 show numpy
+
+#import tensorflow ---> There is no officially version of tersenflow which supports python 3.10 or more
---
+++ Numpy/2.py
@@ -0,0 +1,7 @@
+import numpy as np
+myarr = np.array([[3,6,32,27]], np.int8)
+print(myarr)
+print(myarr[0,1])
+print(myarr[0])
+print(myarr.shape)
+print(myarr.dtype)
